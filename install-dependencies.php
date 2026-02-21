4bf69b2171645a700c56e4e9e3cdad229e8c987c
Ange Djedjed <djedjedange20@gmail.com>
2026-02-21 11:07:29 +0100

    new

--- install-dependencies.php
+++ install-dependencies.php
@@ -13,7 +13,7 @@ set_time_limit(600); // 10 minutes
 ini_set('memory_limit', '512M');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL); 
 
 echo "<!DOCTYPE html>
 <html>
